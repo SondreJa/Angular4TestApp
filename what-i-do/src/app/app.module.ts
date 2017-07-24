1c9b4120bf811cf47a8d2d28f48618990eb20fc1
SondreJa <sondreja@online.no>
2017-07-24 21:00:44 +0200

    Login and messaging, unable to resolve firebase error for signout

--- what-i-do/src/app/app.module.ts
+++ what-i-do/src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { AngularFireModule} from 'angularfire2'
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC9BrCKgsllux1urk0a57NnYVBobxJAUnA",
+  authDomain: "newprojectfortestingng.firebaseapp.com",
+  databaseURL: "https://newprojectfortestingng.firebaseio.com",
+  projectId: "newprojectfortestingng",
+  storageBucket: "",
+  messagingSenderId: "111757140213"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
